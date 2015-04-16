f1e6d3a6ad4febb9401a4fed19e5c96d1c79a8d3
sachintsany <sachintsany@gmail.com>
2015-04-16 06:33:31 +0530

    Database Design

---
+++ DBClass.php
@@ -0,0 +1,66 @@
+
+<?php
+
+//Connecting to MySQL database
+//Use functions query, select and quote functions as and when needed
+class DBClass 
+{
+
+	protected static $connection;
+
+//Return mysqli object on success, false on failure
+	public function connect()
+    {
+		if(!isset(self::$connection)) 
+		{
+			$config = parse_ini_file('./config.ini');
+			self::$connection = new mysqli($config['host'],$config['username'],$config['password'],$config['dbname']);
+		}
+		if(self::$connection === false) 
+		{
+			return false;
+		}
+		return self::$connection;
+	}
+
+//Return mysqli_result object on success, false on failure
+	public function query($query) 
+	{
+		$con = $this -> connect();
+		if($con===false)
+			die("Couldn't connect to database");
+		$result = $con -> query($query);
+		return $result;
+	}
+	
+//Return associative array of rows on success, false on failure
+	public function select($query) 
+	{
+		$rows = array();
+		$result = $this -> query($query);
+		if($result === false) {
+			return false;
+		}
+		while ($row = $result -> fetch_assoc()) 
+		{
+			$rows[] = $row;
+		}
+		return $rows;
+	}	
+	
+//Return last error from daatbase as string	
+	public function error() 
+	{
+		$con = $this -> connect();
+		return $con -> error;
+	}
+	
+//Create a legal SQL string for use in an SQL statement; Return the escaped string	
+	public function quote($value) {
+		$connection = $this -> connect();
+		return "'" . $connection -> real_escape_string($value) . "'";
+	}	
+
+}
+
+?>
---
+++ sqltable.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Create tables</title>
+</head>
+
+<body>
+
+<?php
+
+$
+
+$SystemDB= <<<HERE
+CREATE DATABASE CounsSystem
+HERE;
+
+$Counselor= <<<HERE
+CREATE TABLE Counselor
+(fname VARCHAR(15) NOT NULL,
+lname VARCHAR(15) NOT NULL,
+mailid VARCHAR(15),
+password VARCHAR(15) NOT NULL,
+PRIMARY KEY(mailid));
+HERE;
+             
+$Administrator= <<<HERE
+CREATE TABLE Administrator
+fname VARCHAR(15) NOT NULL,
+lname VARCHAR(15) NOT NULL,
+mailid VARCHAR(15),
+password VARCHAR(15) NOT NULL,
+PRIMARY KEY(mailid));  
+HERE;
+
+$Applicant= <<<HERE
+CREATE TABLE Applicant
+(fname VARCHAR(15) NOT NULL,
+lname VARCHAR(15) NOT NULL,
+mailid VARCHAR(15),
+password VARCHAR(15) NOT NULL,
+mailidCouns VARCHAR(15)
+PRIMARY KEY(mailid),
+FOREIGN KEY(mailidCouns) REFERNCES Counselor(mailid));
+HERE;
+
+// BLOB-upto 64KB; MEDIUMBLOB-upto 16 MB
+
+$Note= <<<HERE
+CREATE TABLE Note
+(note MEDIUMBLOB,
+mailidAppl NOT NULL,
+PRIMARY KEY(mailidAppl),
+FOREIGN KEY(mailidAppl) REFERENCES Applicant(mailid));
+HERE;
+
+$Docs= <<<HERE
+CREATE TABLE Docs
+(resume MEDIUMBLOB,
+applEssay MEDIUMBLOB,
+recomLetter MEDIUMBLOB,
+otherDocs MEDIUMBLOB,
+mailidAppl NOT NULL,
+PRIMARY KEY(mailidAppl),
+FOREIGN KEY(mailidAppl) REFERENCES Applicant(mailid));
+HERE;
+
+//DATE format: YYYY:MM:DD; YEAR: YYYY. GRE score, TOEFL score-integer/float?
+
+$Profile= <<<HERE
+CREATE TABLE Profile
+(dob DATE,
+college VARCHAR(40),
+branch VARCHAR(40),
+yearofComp YEAR,
+gpa float,
+greScore float,
+toeflScore float,
+mailidAppl NOT NULL,
+PRIMARY KEY(mailidAppl),
+FOREIGN KEY(mailidAppl) REFERENCES Applicant(mailid));
+HERE;
+
+//TIME format: HH:MM:SS
+
+$Message= <<<HERE
+CREATE TABLE Message
+(sender CHAR(1) NOT NULL,
+content BLOB,
+date DATE NOT NULL,
+time TIME NOT NULL,
+mailidAppl NOT NULL,
+PRIMARY KEY(sender,time,date,mailidAppl),
+FOREIGN KEY(mailidAppl) REFERENCES Applicant(mailid));
+HERE;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
